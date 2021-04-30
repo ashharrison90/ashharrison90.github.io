3356bb5169879a08bad6dc17a68bcd078bee7211
ashharrison90 <ashharrison90@gmail.com>
2021-04-30 13:55:45 +0000

    Deploying to gh-pages from @ ashharrison90/site@e5dcd9ff76d1db1dfeedc5dc2a6a87e95e3d8c96 🚀

---
+++ _next/static/b3l-Mgl7oO6QQH525CzjG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f9b342d7aa992dfe1f1f.css","static/chunks/pages/index-1b2ccfaa563532f33fbf.js"],"/_error":["static/chunks/pages/_error-b62f22e730a9083a35c3.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
