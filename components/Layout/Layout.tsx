81c1dfc7836bc4424b65c2cf54ede3f641635370
Ashley Harrison <ashharrison90@gmail.com>
2021-05-06 16:38:46 +0100

    refactor parallax container into layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -5,17 +5,47 @@ import styles from './Layout.module.scss'
 
 export interface Props {
   buildTimestamp: number,
-  children: ReactNode
+  children: ReactNode,
+  showHero?: boolean
 }
 
-export default function Layout({ buildTimestamp, children }: Props) {
+const test = () => {
+  document.querySelector(`.${styles.content}`)?.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+export default function Layout({ buildTimestamp, children, showHero = false }: Props) {
   return (
     <div className={styles.container}>
       <Header />
-      <div className={styles.content}>{children}</div>
-      <Footer
-        footnote={`Last updated: ${new Date(buildTimestamp).toLocaleDateString()}`}
-      />
+      <div className={styles.parallaxContainer}>
+        {
+          showHero &&
+            <>
+              <div className={styles.heroBackground} />
+              <div className={styles.heroCutout} />
+            </>
+        }
+        <div className={styles.foreground}>
+          {
+            showHero &&
+              <div className={styles.heroTitleContainer}>
+                <h1 className={styles.heroTitle}>
+                  hi<br/>
+                  i'm ash
+                </h1>
+                <button className={styles.goToContent} onClick={test}>Scroll</button>
+              </div>
+          }
+          <div className={styles.contentContainer}>
+            <div className={styles.content}>{children}</div>
+          </div>
+          <Footer
+            footnote={`Last updated: ${new Date(buildTimestamp).toLocaleDateString()}`}
+          />
+        </div>
+      </div>
     </div>
   )
 }
