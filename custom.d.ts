302d53fbbf4f750d7571cc949a288521d6896fbe
Ashley Harrison <ashharrison90@gmail.com>
2021-05-07 11:41:51 +0100

    styling for images + code blocks in posts

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,8 @@ declare module '*.svg' {
   const content: any;
   export default content;
 }
+
+declare module 'highlight.js/lib/core'
+declare module 'highlight.js/lib/languages/javascript'
+declare module 'highlight.js/lib/languages/typescript'
+declare module 'highlight.js/lib/languages/python'
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,11 +1,20 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
 import Head from 'next/head'
+import { useEffect } from 'react';
 import { getPostBySlug, getAllPosts, PostData } from '../../lib/postsApi'
 import markdownToHtml from '../../lib/markdownToHtml'
 import { GetStaticPropsContext } from 'next'
 import Layout from '../../components/Layout/Layout'
 import styles from '../../styles/[slug].module.scss'
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+import python from 'highlight.js/lib/languages/python';
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('python', python);
+import 'highlight.js/styles/monokai-sublime.css';
 
 export interface Props {
   buildTimestamp: number,
@@ -13,6 +22,9 @@ export interface Props {
 }
 
 export default function Post({ buildTimestamp, post }: Props) {
+  useEffect(() => {
+    hljs.highlightAll()
+  }, [])
   const router = useRouter()
   if (!router.isFallback && !post.slug) {
     return <ErrorPage statusCode={404} />
@@ -34,6 +46,7 @@ export default function Post({ buildTimestamp, post }: Props) {
             <div className={styles.date}>{(new Date(post.date!)).toLocaleDateString()}</div>
           </div>
           <div
+            className={styles.content}
             dangerouslySetInnerHTML={{ __html: post.content ?? '' }}
           />
         </Layout>
