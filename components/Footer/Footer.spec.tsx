177c6b89f7fed2ec1778bb2c4cffb9c5160f3d82
Ashley Harrison <ashharrison90@gmail.com>
2021-05-28 20:39:25 +0100

    add test workflow

--- components/Footer/Footer.spec.tsx
+++ components/Footer/Footer.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import Footer from './Footer'
 
 describe('Footer', () => {
-  it('has links', () => {
+  it('has various social links', () => {
     render(<Footer />)
     const links = screen.getAllByRole('link')
     expect(links.length).toEqual(6)
