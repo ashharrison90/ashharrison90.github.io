e5a712bba76427139b00c751461601235f2de16b
Ashley Harrison <ashharrison90@gmail.com>
2021-07-02 21:56:36 +0100

    turn on lighthouse assertions, ignore 3 for now

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,13 @@
 module.exports = {
   ci: {
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'csp-xss': 'off',
+        'non-composited-animations': 'off',
+        'unused-javascript': 'off',
+      },
+    },
     collect: {
       staticDistDir: './out',
       url: ['http://localhost:8080'],
