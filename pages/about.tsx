1ee39c250e78c15a09cec96e9c59b3f183576707
Ashley Harrison <ashharrison90@gmail.com>
2021-07-03 11:40:52 +0100

    add skills to the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,23 @@
 import Head from 'next/head'
+import Typewriter, { Options, TypewriterClass } from 'typewriter-effect'
 import { getAllJobs, JobData } from '../lib/jobsApi'
 import Layout from '../components/Layout/Layout'
 import styles from '../styles/About.module.scss'
 import JobSummary from '../components/JobSummary/JobSummary'
 import markdownToHtml from '../lib/markdownToHtml'
+import skills from '../public/assets/about/skills.json'
 import Ibm from '../public/assets/about/ibm.svg'
 import Qinetiq from '../public/assets/about/qinetiq.svg'
 import Durham from '../public/assets/about/durham.svg'
 import Grafana from '../public/assets/about/grafana.svg'
 import { ReactNode } from 'react'
+
+// Need to extend Typewriter's types until https://github.com/tameemsafi/typewriterjs/pull/106 is merged
+const TypewriterWithComponent = Typewriter as React.FunctionComponent<{
+  component?: string
+  onInit?: (typewriter: TypewriterClass) => void
+  options?: Partial<Options>
+}>
 
 const iconMap: Record<string, ReactNode> = {
   durham: <Durham />,
@@ -31,13 +40,34 @@ export default function About({ jobs }: Props) {
 
       <h1 className={styles.title}>about</h1>
 
+      <p>
+        I specialise in all things frontend, with a focus on{' '}
+        <strong>clean</strong>, <strong>testable</strong> and{' '}
+        <strong>maintainable</strong> code.
+        <br />I work with{' '}
+        <TypewriterWithComponent
+          component='span'
+          options={{
+            cursor: '',
+            loop: true,
+            wrapperClassName: styles.typewriter,
+          }}
+          onInit={(typewriter) => {
+            skills.forEach((skill) => {
+              typewriter.typeString(`${skill}`)
+              typewriter.pauseFor(500)
+              typewriter.deleteAll()
+            })
+            typewriter.start()
+          }}
+        />
+      </p>
       <q>
         Find a job you enjoy doing, and you will never have to work a day in
         your life.
       </q>
       <cite> - Mark Twain</cite>
       <p>But did Mark Twain ever develop software? No. Stupid.</p>
-
       {jobs.map((job) => (
         <JobSummary
           key={job.company}
--- pages/index.tsx
+++ pages/index.tsx
@@ -96,11 +96,6 @@ export default function Home({ allPosts }: Props) {
             <h2>about</h2>
             <p>I'm a software engineer based in the UK.</p>
             <p>
-              I specialize in all things frontend, with a focus on{' '}
-              <strong>clean</strong>, <strong>testable</strong> and{' '}
-              <strong>maintainable</strong> code.
-            </p>
-            <p>
               When I'm not typing on a keyboard, you can find me{' '}
               <s>typing on a keyboard</s> gaming, playing football and hanging
               out with friends. Gym or mountain biking if I'm feeling motivated.
