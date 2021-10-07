8690cf890fc9b877940a7ecf709d8e88f6f24f55
Ashley Harrison <ashharrison90@gmail.com>
2021-10-07 16:46:44 +0100

    update to use @playwright/test (#94)
    * update to use @playwright/test
    
    * this needs to be install-deps on the CI

--- jest-e2e.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  preset: 'jest-playwright-preset',
-  testMatch: ['**/__test__/e2e/**/*.spec.ts'],
-  testEnvironmentOptions: {
-    'jest-playwright': {
-      browsers: ['chromium', 'firefox', 'webkit'],
-    },
-  },
-}
