bf2ad54df629a42a042bf3ad702152167bc9eadd
Ashley Harrison <ashley.harrison@grafana.com>
2022-01-23 13:44:23 +0000

    turn off non-composited-animations check

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,6 +3,7 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
+        'non-composited-animations': 'off', // disable this for now
         'csp-xss': 'off', // can't set the csp effectively with a static site in github pages
         'unused-javascript': 'off', // nextjs prefetching pages
         'uses-responsive-images': ['error', { maxLength: 1 }], // hero cutout image
