9df60a4ec5b55a244083780dad0c2dfa74664d21
Ashley Harrison <ashley.harrison@grafana.com>
2022-04-16 15:06:58 +0100

    Add utteranc.es for comments on posts (#138)
    * Add utteranc.es for comments on posts
    
    * fix label

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import { AppProps } from 'next/app'
-import { ThemeContextProvider } from '../context/ThemeContext/ThemeContext'
+import dynamic from 'next/dynamic'
 import '../styles/globals.scss'
+
+// Turn off SSR for the theme context so we can access window immediately
+const ThemeContextProvider = dynamic(
+  () => import('../context/ThemeContext/ThemeContext'),
+  {
+    ssr: false,
+  }
+)
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
