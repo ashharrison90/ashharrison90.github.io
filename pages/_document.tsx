1a1ef7d9cd8760a82e57e9a090a2ae79679e43d4
Ashley Harrison <ashley.harrison@grafana.com>
2022-08-20 12:18:44 +0100

    use next/script (thanks lighthouse)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
@@ -9,12 +10,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en-GB'>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300&family=Lora:ital@0;1&family=Rubik:wght@700&family=IBM+Plex+Mono:wght@300&display=swap'
-          rel='stylesheet'
-        />
+        <Script>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300&family=Lora:ital@0;1&family=Rubik:wght@700&family=IBM+Plex+Mono:wght@300&display=swap'
+            rel='stylesheet'
+          />
+        </Script>
         <Head>
           <link rel='icon' type='image/svg' href='/favicon.svg' />
         </Head>
