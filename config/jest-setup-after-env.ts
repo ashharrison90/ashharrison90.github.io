0b2a7460c5f776227b3860dad055c1fe5c8f338a
Ashley Harrison <ashley.harrison@grafana.com>
2023-11-19 11:38:50 +0000

    fix some unit tests

---
+++ config/jest-setup-after-env.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'
+import failOnConsole from 'jest-fail-on-console'
+
+failOnConsole({
+  shouldFailOnLog: true,
+})
--- config/jest-setup.ts
+++ config/jest-setup.ts
@@ -1,6 +1,1 @@
-import '@testing-library/jest-dom'
-import failOnConsole from 'jest-fail-on-console'
-
-failOnConsole({
-  shouldFailOnLog: true,
-})
+window.scrollTo = jest.fn()
